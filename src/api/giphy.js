a91d43dd542db7a20f960f3bb9d43186cba43a9d
Andrius Dadašev <33958544+dadasev@users.noreply.github.com>
2021-02-08 10:14:38 +0200

    Update giphy.js

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'ldIU3wYEvQopU2WVPSbB0N2w5byGTWnB';
 
 export default axios.create({
-  baseURL: 'http://api.giphy.com/v1/gifs',
+  baseURL: 'https://api.giphy.com/v1/gifs',
   params: {
     limit: 12,
     api_key: KEY,
